5bce2795b3ec0d91ecc42a77bd630d7f56dfc594
Roman Gulevich <r.i.gulevich@gmail.com>
2023-01-31 13:19:19 +0300

    Added implementations

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -1,7 +1,9 @@
 package com.mjc.stage2.impl;
 
 
-public class ProxyConnection {
+import com.mjc.stage2.Connection;
+
+public class ProxyConnection implements Connection {
     private RealConnection realConnection;
 
     public ProxyConnection(RealConnection realConnection) {
@@ -9,7 +11,20 @@ public class ProxyConnection {
     }
 
     public void reallyClose() {
+        if (!realConnection.isClosed()) {
+            realConnection.close();
+        }
         // Write your code here!
+    }
+
+    @Override
+    public void close() {
+        realConnection.close();
+    }
+
+    @Override
+    public boolean isClosed() {
+        return realConnection.isClosed();
     }
     // Implement methods here!
 }
